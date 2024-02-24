7bb8be1989d32d5622d341d290c85e8f209b97a5
witchbl4des <menzo.smit-adam@epitech.eu>
2024-02-24 22:40:02 +0100

    Add bcrypt and bcryptjs dependencies, update devDependencies, and implement user registration

--- src/actions/register.tsx
+++ src/actions/register.tsx
@@ -1,7 +1,10 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcryptjs";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas/index"
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validateFields = RegisterSchema.safeParse(values);
@@ -10,5 +13,21 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         return { error: "Invalid fields" };
     }
 
-    return { success: "email sent" };
+    const { email, password, name } = validateFields.data;
+    const passwdHash = await bcrypt.hash(password, 10)
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+        return { error: "user already exists" };
+    }
+
+    await db.user.create({
+        data: {
+            name, email, password: passwdHash
+        },
+    })
+
+
+
+    return { success: "user created !" };
 };
